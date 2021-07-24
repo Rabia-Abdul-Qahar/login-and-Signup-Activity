58bea8865e8121b66f8bd11639b6b8838a32daf8
Rabia-Rajput <rabiarajput0300@gmail.com>
2021-07-24 16:48:03 +0500

    Sign-Up Activity, Login Activity and Info Activity.

---
+++ app/src/main/java/com/firstapp/p2t1/Login.java
@@ -0,0 +1,24 @@
+package com.firstapp.p2t1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn =findViewById(R.id.TextVewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Register.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/firstapp/p2t1/Register.java
@@ -0,0 +1,26 @@
+package com.firstapp.p2t1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        TextView btn =findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this,Login.class));
+            }
+        });
+    }
+}
